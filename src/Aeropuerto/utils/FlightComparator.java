6ece0027b1afdda79a3e2094102bf423970f7639
AngelQuinga <angel.quinga@epn.edu.ec>
2026-01-30 13:09:19 -0500

    Actualizar flightComparator

--- src/Aeropuerto/utils/FlightComparator.java
+++ src/Aeropuerto/utils/FlightComparator.java
@@ -4,17 +4,19 @@ import Aeropuerto.modelo.Flight;
 import java.util.Comparator;
 
 public class FlightComparator implements Comparator<Flight> {
+
     @Override
     public int compare(Flight f1, Flight f2) {
-        // 1. Prioridad: Emergencia (true va antes que false)
-        if (f1.isEmergency() && !f2.isEmergency()) return -1;
-        if (!f1.isEmergency() && f2.isEmergency()) return 1;
 
-        // 2. Prioridad: Tiempo de llegada (menor timestamp va antes)
+        // 1. Emergencias primero
+        int emergencyCompare = Boolean.compare(f2.isEmergency(), f1.isEmergency());
+        if (emergencyCompare != 0) return emergencyCompare;
+
+        // 2. Llega antes = mayor prioridad
         int timeCompare = Long.compare(f1.getTimestamp(), f2.getTimestamp());
         if (timeCompare != 0) return timeCompare;
 
-        // 3. Desempate: ID alfabético
+        // 3. Desempate por ID
         return f1.getId().compareTo(f2.getId());
     }
 }
